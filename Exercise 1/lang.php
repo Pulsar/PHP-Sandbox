c049903cee093889545cef2e90c8f1b8fb9a7731
Pulsar <gjergjlala@gmx.de>
2018-05-25 23:57:37 +0200

    Check send form and answer accordingly

--- Exercise 1/lang.php
+++ Exercise 1/lang.php
@@ -15,6 +15,21 @@
 		<input type="radio" name="value" value="no_C"> No, I'm not interested in C<br>
 		<input type="submit" value="Send">
 	</form>
+<?php
+	// Check if value is defined -> if there is no post, the value is not defined
+	// then it gives an error
+	$C = isset($_POST['value']) ? $_POST['value'] : "";
+	
+	// One could use $_POST instead of $C
+	if (isset($C) && $C=="C_know"){
+		echo "Thats Great!";
+	} elseif (isset($C) && $C=="C_learn"){
+		echo "You will learn C in no time here!";
+	} elseif (isset($C) && $C=="no_C"){
+		echo "Not interested in C?";
+	} else{
+		echo "Question not send!";	
+	}
 ?>
 </body>
 </html>
