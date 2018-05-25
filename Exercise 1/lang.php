1614d513f2c7f785636e4873b8241a5174f00d71
Pulsar <gjergjlala@gmx.de>
2018-05-25 23:54:50 +0200

    Add a proper introductory question

--- Exercise 1/lang.php
+++ Exercise 1/lang.php
@@ -4,24 +4,8 @@
 <meta charset="utf-8"></meta>
 </head>
 <body>
-
 <?php
-
-echo "begin ";
-if (isset($C) && $C=="C_know")
-  echo "you know C well";
- elseif (isset($C) && $C=="C_learn")
-    echo "you will learn C";
-
-    else
-      echo "no C";
-
-
-
-
-echo " end";
-
-
+	echo "What do you think about C programming language?<br>";
 ?>
 </body>
 </html>
