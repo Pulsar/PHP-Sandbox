28b967887e50bfc92058e31f215f2df00f9b8ee5
Pulsar <gjergjlala@gmx.de>
2018-05-25 23:56:45 +0200

    Add input form
    Simple optiosn for answering the question.

--- Exercise 1/lang.php
+++ Exercise 1/lang.php
@@ -7,5 +7,14 @@
 <?php
 	echo "What do you think about C programming language?<br>";
 ?>
+	<!-- We need to define the variable before using it -->
+	<!-- Therefore we use post to set the variable -->
+	<form method="post" action="">
+		<input type="radio" name="value" value="C_know" checked> I already know C<br>
+		<input type="radio" name="value" value="C_learn"> I want to learn C<br>
+		<input type="radio" name="value" value="no_C"> No, I'm not interested in C<br>
+		<input type="submit" value="Send">
+	</form>
+?>
 </body>
 </html>
